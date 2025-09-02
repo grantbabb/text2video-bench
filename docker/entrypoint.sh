ef6a417a718d7149ac72e565e6f6f535bdfc0656
grantbabb <grantbabb@users.noreply.github.com>
2025-09-01 21:03:13 -0700

    test entry func

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Enter command to generate video."
-    exit 1
-fi
 
-python -u "$1" < /dev/tty
+
+python -m scripts.animate --config configs/prompts/1_animate/1_1_animate_RealisticVision.yaml
